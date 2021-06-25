be929f77fcc4d79398c800c2dfc9ca119f6a6485
Vinay Kumar <76948148+vinay-17@users.noreply.github.com>
2021-06-26 00:01:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript - React.js</h1>
+      <p>
+        A basic web development on React Js which helps to propagate Web-Dev
+        skills.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
